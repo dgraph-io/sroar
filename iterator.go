456e099972f46004eb012b05d9ddea2ff2324fd7
Ahsan Barkati <ahsanbarkati@gmail.com>
2021-08-15 01:42:35 +0530

    Implement fast iterator and range iterators

--- iterator.go
+++ iterator.go
@@ -16,6 +16,10 @@
 
 package sroar
 
+import (
+	"math/bits"
+)
+
 type Iterator struct {
 	index   int
 	reverse bool
@@ -71,6 +75,87 @@ func (itr *Iterator) AdvanceIfNeeded(minval uint64) {
 			break
 		}
 	}
+}
+
+type FastIterator struct {
+	bm *Bitmap
+
+	keys []uint64
+	kidx int
+
+	cidx int
+
+	bmHiIdx int
+	bitset  uint16
+}
+
+func (bm *Bitmap) NewRangeIterators(numRanges int) []*FastIterator {
+	keyn := bm.keys.numKeys()
+	iters := make([]*FastIterator, numRanges)
+	width := keyn / numRanges
+
+	for i := 0; i < numRanges; i++ {
+		iters[i] = bm.NewFastIterator()
+		if i == numRanges-1 {
+			iters[i].keys = iters[i].keys[width*i:]
+		}
+		iters[i].keys = iters[i].keys[width*i : width*(i+1)]
+	}
+	return iters
+}
+
+func (bm *Bitmap) NewFastIterator() *FastIterator {
+	return &FastIterator{
+		bm:      bm,
+		keys:    bm.keys[indexNodeStart : indexNodeStart+bm.keys.numKeys()*2],
+		kidx:    0,
+		cidx:    -1,
+		bmHiIdx: -1,
+	}
+}
+
+func (it *FastIterator) Next() uint64 {
+	if len(it.keys) == 0 {
+		return 0
+	}
+	key := it.keys[2*it.kidx]
+	off := it.keys[2*it.kidx+1]
+	cont := it.bm.getContainer(off)
+	card := getCardinality(cont)
+
+	// we need to jump container in these scenarios
+	// - The cardinality of this container is zero
+	// - cidx is already at the last element of the container
+	for card == 0 || it.cidx+1 >= card {
+		if it.kidx+1 >= len(it.keys)/2 {
+			return 0
+		}
+		it.kidx++
+		it.cidx = -1
+		it.bmHiIdx = -1
+		it.bitset = 0
+		key = it.keys[2*it.kidx]
+		off = it.keys[2*it.kidx+1]
+		cont = it.bm.getContainer(off)
+	}
+
+	//  The above loop assures that we can do next in this container
+	it.cidx++
+	switch cont[indexType] {
+	case typeArray:
+		return key | uint64(cont[int(startIdx)+it.cidx])
+	case typeBitmap:
+		for it.bitset == 0 && it.bmHiIdx+1 < len(cont[startIdx:]) {
+			it.bmHiIdx++
+			it.bitset = cont[int(startIdx)+it.bmHiIdx]
+		}
+		assert(it.bitset > 0)
+		loIdx := uint16(bits.LeadingZeros16(it.bitset))
+		msb := 1 << (16 - loIdx - 1)
+		it.bitset ^= uint16(msb)
+		return key | uint64(it.bmHiIdx*16+int(loIdx))
+	}
+	return 0
 }
 
 type ManyItr struct {
--- iterator_test.go
+++ iterator_test.go
@@ -17,109 +17,173 @@
 package sroar
 
 import (
-	"fmt"
-	"math"
 	"math/rand"
+	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestIteratorBasic(t *testing.T) {
-	testSz := []int{0, 1, 16, 2047, 2048, 1e4}
-
-	var sz int
-	test := func(t *testing.T) {
-		b := NewBitmap()
-		for i := uint64(0); i < uint64(sz); i++ {
-			b.Set(i)
-		}
-		it := b.NewIterator()
-		cnt := uint64(0)
-		for it.HasNext() {
-			require.Equal(t, cnt, it.Next())
-			cnt++
-		}
-		require.Equal(t, uint64(sz), cnt)
-
-		rit := b.NewReverseIterator()
-		for rit.HasNext() {
-			cnt--
-			require.Equal(t, cnt, rit.Next())
-		}
-		require.Equal(t, uint64(0), cnt)
-	}
-	for i := range testSz {
-		sz = testSz[i]
-		t.Run(fmt.Sprintf("test-%d", sz), test)
-	}
-
-	r := rand.New(rand.NewSource(0))
-	t.Run("test-random", func(t *testing.T) {
-		b := NewBitmap()
-		N := uint64(1e4)
-		for i := uint64(0); i < N; i++ {
-			b.Set(uint64(r.Int63n(math.MaxInt64)))
-		}
-		it := b.NewIterator()
-		var vals []uint64
-		for it.HasNext() {
-			vals = append(vals, it.Next())
-		}
-		require.Equal(t, b.ToArray(), vals)
-	})
-}
-
-func TestIteratorWithRemoveKeys(t *testing.T) {
-	b := NewBitmap()
-	N := uint64(1e6)
-	for i := uint64(0); i < N; i++ {
-		b.Set(i)
-	}
-
-	b.RemoveRange(0, N)
-	it := b.NewIterator()
-
-	cnt := 0
-	for it.HasNext() {
-		cnt++
-		it.Next()
-	}
-	require.Equal(t, 0, cnt)
-}
-
-func TestManyIterator(t *testing.T) {
-	b := NewBitmap()
-	for i := 0; i < int(1e6); i++ {
-		b.Set(uint64(i))
-	}
-
-	mi := b.ManyIterator()
-	buf := make([]uint64, 1000)
-
-	i := 0
-	for {
-		got := mi.NextMany(buf)
-		if got == 0 {
-			break
-		}
-		require.Equal(t, 1000, got)
-		require.Equal(t, uint64(i*1000), buf[0])
-		i++
-	}
-}
-
-func BenchmarkIterator(b *testing.B) {
+	n := uint64(1e5)
 	bm := NewBitmap()
-	for i := 0; i < int(1e5); i++ {
+	for i := uint64(1); i <= n; i++ {
 		bm.Set(uint64(i))
 	}
 
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		it := bm.NewIterator()
-		for it.HasNext() {
-			it.Next()
+	it := bm.NewFastIterator()
+	for i := uint64(1); i <= n; i++ {
+		v := it.Next()
+		require.Equal(t, i, v)
+	}
+	v := it.Next()
+	require.Equal(t, uint64(0), v)
+}
+
+func TestIteratorRanges(t *testing.T) {
+	n := uint64(1e5)
+	bm := NewBitmap()
+	for i := uint64(1); i <= n; i++ {
+		bm.Set(uint64(i))
+	}
+
+	iters := bm.NewRangeIterators(8)
+	cnt := uint64(1)
+	for idx := 0; idx < 8; idx++ {
+		it := iters[idx]
+		for v := it.Next(); v > 0; v = it.Next() {
+			// t.Log(cnt, v)
+			require.Equal(t, cnt, v)
+			cnt++
 		}
 	}
 }
+
+func TestIteratorRandom(t *testing.T) {
+	n := uint64(1e6)
+	bm := NewBitmap()
+	mp := make(map[uint64]struct{})
+	var arr []uint64
+	for i := uint64(1); i <= n; i++ {
+		v := uint64(rand.Intn(int(n) * 5))
+		if v == 0 {
+			continue
+		}
+		if _, ok := mp[v]; ok {
+			continue
+		}
+		mp[v] = struct{}{}
+		arr = append(arr, v)
+		bm.Set(uint64(v))
+	}
+
+	sort.Slice(arr, func(i, j int) bool {
+		return arr[i] < arr[j]
+	})
+
+	it := bm.NewFastIterator()
+	v := it.Next()
+	for i := uint64(0); i < uint64(len(arr)); i++ {
+		require.Equal(t, arr[i], v)
+		v = it.Next()
+	}
+}
+
+// func TestIteratorBasic(t *testing.T) {
+// 	testSz := []int{0, 1, 16, 2047, 2048, 1e4}
+
+// 	var sz int
+// 	test := func(t *testing.T) {
+// 		b := NewBitmap()
+// 		for i := uint64(0); i < uint64(sz); i++ {
+// 			b.Set(i)
+// 		}
+// 		it := b.NewIterator()
+// 		cnt := uint64(0)
+// 		for it.HasNext() {
+// 			require.Equal(t, cnt, it.Next())
+// 			cnt++
+// 		}
+// 		require.Equal(t, uint64(sz), cnt)
+
+// 		rit := b.NewReverseIterator()
+// 		for rit.HasNext() {
+// 			cnt--
+// 			require.Equal(t, cnt, rit.Next())
+// 		}
+// 		require.Equal(t, uint64(0), cnt)
+// 	}
+// 	for i := range testSz {
+// 		sz = testSz[i]
+// 		t.Run(fmt.Sprintf("test-%d", sz), test)
+// 	}
+
+// 	r := rand.New(rand.NewSource(0))
+// 	t.Run("test-random", func(t *testing.T) {
+// 		b := NewBitmap()
+// 		N := uint64(1e4)
+// 		for i := uint64(0); i < N; i++ {
+// 			b.Set(uint64(r.Int63n(math.MaxInt64)))
+// 		}
+// 		it := b.NewIterator()
+// 		var vals []uint64
+// 		for it.HasNext() {
+// 			vals = append(vals, it.Next())
+// 		}
+// 		require.Equal(t, b.ToArray(), vals)
+// 	})
+// }
+
+// func TestIteratorWithRemoveKeys(t *testing.T) {
+// 	b := NewBitmap()
+// 	N := uint64(1e6)
+// 	for i := uint64(0); i < N; i++ {
+// 		b.Set(i)
+// 	}
+
+// 	b.RemoveRange(0, N)
+// 	it := b.NewIterator()
+
+// 	cnt := 0
+// 	for it.HasNext() {
+// 		cnt++
+// 		it.Next()
+// 	}
+// 	require.Equal(t, 0, cnt)
+// }
+
+// func TestManyIterator(t *testing.T) {
+// 	b := NewBitmap()
+// 	for i := 0; i < int(1e6); i++ {
+// 		b.Set(uint64(i))
+// 	}
+
+// 	mi := b.ManyIterator()
+// 	buf := make([]uint64, 1000)
+
+// 	i := 0
+// 	for {
+// 		got := mi.NextMany(buf)
+// 		if got == 0 {
+// 			break
+// 		}
+// 		require.Equal(t, 1000, got)
+// 		require.Equal(t, uint64(i*1000), buf[0])
+// 		i++
+// 	}
+// }
+
+// func BenchmarkIterator(b *testing.B) {
+// 	bm := NewBitmap()
+// 	for i := 0; i < int(1e5); i++ {
+// 		bm.Set(uint64(i))
+// 	}
+
+// 	b.ResetTimer()
+// 	for i := 0; i < b.N; i++ {
+// 		it := bm.NewIterator()
+// 		for it.HasNext() {
+// 			it.Next()
+// 		}
+// 	}
+// }
