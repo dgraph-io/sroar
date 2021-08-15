0dc04a81e2b21e84b95fbda5886e1c78da175b97
Ahsan Barkati <ahsanbarkati@gmail.com>
2021-08-15 18:16:06 +0530

    Remove old iterator

--- iterator.go
+++ iterator.go
@@ -20,63 +20,6 @@ import (
 	"math/bits"
 )
 
-type Iterator struct {
-	index   int
-	reverse bool
-	arr     []uint64
-}
-
-func (r *Bitmap) NewIterator() *Iterator {
-	return &Iterator{
-		index: -1,
-		arr:   r.ToArray(),
-	}
-}
-
-func (r *Bitmap) NewReverseIterator() *Iterator {
-	return &Iterator{
-		index:   r.GetCardinality(),
-		arr:     r.ToArray(),
-		reverse: true,
-	}
-}
-
-func (itr *Iterator) HasNext() bool {
-	if itr.reverse {
-		return itr.index > 0
-	} else {
-		return int(itr.index) < len(itr.arr)-1
-	}
-}
-
-func (itr *Iterator) Next() uint64 {
-	if itr.reverse {
-		itr.index--
-
-	} else {
-		itr.index++
-	}
-	return itr.arr[itr.index]
-}
-
-func (itr *Iterator) Val() uint64 {
-	return itr.arr[itr.index]
-}
-
-// AdvanceIfNeeded advances until the value < minval.
-func (itr *Iterator) AdvanceIfNeeded(minval uint64) {
-	if itr.index < 0 {
-		return
-	}
-	for itr.Val() < minval {
-		if itr.HasNext() {
-			itr.Next()
-		} else {
-			break
-		}
-	}
-}
-
 type FastIterator struct {
 	bm *Bitmap
 
@@ -94,8 +37,10 @@ func (bm *Bitmap) NewRangeIterators(numRanges int) []*FastIterator {
 	iters := make([]*FastIterator, numRanges)
 	width := keyn / numRanges
 	rem := keyn % numRanges
-
 	cnt := 0
+
+	// This loop distributes the key equally to the ranges. For example: If numRanges = 3
+	// and keyn = 8 then it will be distributes as [3, 3, 2]
 	for i := 0; i < numRanges; i++ {
 		iters[i] = bm.NewFastIterator()
 		n := width
@@ -105,15 +50,6 @@ func (bm *Bitmap) NewRangeIterators(numRanges int) []*FastIterator {
 		iters[i].keys = iters[i].keys[cnt : cnt+2*n]
 		cnt = cnt + 2*n
 	}
-
-	// for i := 0; i < numRanges; i++ {
-	// 	iters[i] = bm.NewFastIterator()
-	// 	if i == numRanges-1 {
-	// 		iters[i].keys = iters[i].keys[2*width*i:]
-	// 	} else {
-	// 		iters[i].keys = iters[i].keys[2*width*i : 2*width*(i+1)]
-	// 	}
-	// }
 	return iters
 }
 
